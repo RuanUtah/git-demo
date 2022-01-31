2c4fa15bfaf0b368c51180c7fb469210fb668f5b
Ruan Utah <ruan.carvalho@aln.senaicimatec.edu.br>
2022-01-31 18:46:36 -0300

    Changes

---
+++ dicionários.py
@@ -0,0 +1,48 @@
+"""""
+Dicionários
+
+#OBS: em algumas linguagens de prog, os dicionários python são conhecidos por mapas.
+
+Dicionários são coleções do tipo chave/ valor.
+#É um mapeamento entre chave e valor
+
+#Ideia de chave e valor em listas, isto é, índices e valores
+[0,1,2]
+[1,2,3]
+
+#Nos dicionarios, isso fico explicito ao em vez de implícita
+
+Dicionários são representados por chaves {}.
+
+print(type({}))
+
+OBS: Sobre os dicionários
+    - Chaves e Valor são separados por dois pontos 'chave': 'valor'
+    - Tanto chave quanto valor podem ser de quaquer tipo de dado
+    - podemos misturar tipos de dados
+lista = []
+tupla = ()
+dicionario = {}
+------------------------------------------------------------
+
+#Criação de dicionários
+
+#FOrma 1(Mais comum)
+paises = {'br': 'Brasil','eua': 'Estados Unidos', 'py': 'Paraguai'}
+print(paises)
+print(type(paises))
+
+#Forma 2(Menos comum)
+
+paises = dict(br='Brasil', eua = 'Estados Unidos', py = 'Paraguay')
+print(paises)
+print(type(paises))
+------------------------------------------------------------
+
+"""""
+
+#Acessando elementos
+
+
+
+
